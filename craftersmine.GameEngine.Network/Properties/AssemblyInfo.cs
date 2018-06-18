d15c9d2adda4aee2cdf59e0fedb204485d42d787
craftersmine <rodion.morozov.01@gmail.com>
2018-06-18 21:17:49 +0700

    Updated AssemblyInfos

--- craftersmine.GameEngine.Network/Properties/AssemblyInfo.cs
+++ craftersmine.GameEngine.Network/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("craftersmine.GameEngine.Network")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("craftersmine GameEngine Networking Library")]
+[assembly: AssemblyDescription("Provides method to use network in game")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("craftersmine.GameEngine.Network")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCompany("craftersmine")]
+[assembly: AssemblyProduct("craftersmine GameEngine")]
+[assembly: AssemblyCopyright("Copyright © craftersmine 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
