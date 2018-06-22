5a17113aa387e4dc5aef6bbf405223cf0393ab30
craftersmine <rodion.morozov.01@gmail.com>
2018-06-22 18:23:27 +0700

    Added NetworkGameObject base

---
+++ craftersmine.GameEngine.Network/NetworkGameObject.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using craftersmine.GameEngine.System;
+
+namespace craftersmine.GameEngine.Network
+{
+    public class NetworkGameObject : GameObject
+    {
+        public bool IsTransmittingLocation { get; set; }
+        public bool IsReceivingLocation { get; set; }
+        public bool IsSyncingAnimation { get; set; }
+        public bool IsSyncingTexture { get; set; }
+        public bool IsTransmittingSize { get; set; }
+        public bool IsReceivingSize { get; set; }
+
+        public NetworkGameObject(int id, string internalName) : base(id, internalName)
+        {
+
+        }
+
+        public virtual void OnNetworkSync()
+        {
+
+        }
+    }
+}
