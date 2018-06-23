13d03d6cdadadcfa937f48010b4f2dc3e200edb3
craftersmine <rodion.morozov.01@gmail.com>
2018-06-23 15:57:11 +0700

    Added WebRequestHelper

---
+++ craftersmine.GameEngine.Network/WebRequestHelper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+
+namespace craftersmine.GameEngine.Network
+{
+    public sealed class WebRequestHelper
+    {
+        public static string MakeGetRequest(string url)
+        {
+            WebClient webClient = new WebClient();
+            return webClient.DownloadString(url);
+        }
+
+        public static string MakePostRequest(string url, string data)
+        {
+            WebRequest req = WebRequest.Create(url);
+            req.Method = "POST";
+            req.Timeout = 100000;
+            req.ContentType = "application/x-www-form-urlencoded";
+            byte[] sentData = Encoding.GetEncoding(1251).GetBytes(data);
+            req.ContentLength = sentData.Length;
+            Stream sendStream = req.GetRequestStream();
+            sendStream.Write(sentData, 0, sentData.Length);
+            sendStream.Close();
+            WebResponse res = req.GetResponse();
+            Stream ReceiveStream = res.GetResponseStream();
+            StreamReader sr = new StreamReader(ReceiveStream, Encoding.UTF8);
+            Char[] read = new Char[256];
+            int count = sr.Read(read, 0, 256);
+            string Out = String.Empty;
+            while (count > 0)
+            {
+                String str = new String(read, 0, count);
+                Out += str;
+                count = sr.Read(read, 0, 256);
+            }
+            return Out;
+        }
+    }
+}
