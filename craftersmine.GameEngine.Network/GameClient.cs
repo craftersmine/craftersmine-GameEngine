293b906ea816adb04896009829a0774b4505bfb0
craftersmine <rodion.morozov.01@gmail.com>
2018-06-23 17:29:58 +0700

    Added GameClient.cs

---
+++ craftersmine.GameEngine.Network/GameClient.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebSocketSharp;
+
+namespace craftersmine.GameEngine.Network
+{
+    public sealed class GameClient
+    {
+        private WebSocket client;
+
+        public string ServerAddress { get; internal set; }
+        public int ServerPort { get; internal set; }
+
+        public void Connect(string address, int port)
+        {
+
+        }
+    }
+}
