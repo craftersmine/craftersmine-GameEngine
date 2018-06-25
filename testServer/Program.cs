20fbff99d023b3e0dc09ec7f903cda708076ad49
craftersmine <rodion.morozov.01@gmail.com>
2018-06-26 01:20:08 +0700

    Added test server application

---
+++ testServer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using craftersmine.GameEngine.Network;
+
+namespace testServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GameServer gameServer = new GameServer("0.0.0.0", 2000);
+            gameServer.StartServer();
+            Console.ReadLine();
+        }
+    }
+}
