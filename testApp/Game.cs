6f7b19fff52ebe1ae2488a71224b2da7df9aff7e
craftersmine <rodion.morozov.01@gmail.com>
2018-07-04 21:58:25 +0700

    Developer changes

--- testApp/Game.cs
+++ testApp/Game.cs
@@ -16,12 +16,15 @@ namespace testApp
 {
     class Game : GameWindow
     {
-        Scene scene = new Scene() { Id = 0 };
+        public static Scene scene = new Scene() { Id = 0 };
         public static ContentStorage cs;
         public static ContentStorage gc1;
         GameObject[] bordersGrads;
+        craftersmine.GameEngine.System.UI.Label label = new craftersmine.GameEngine.System.UI.Label("YAY label test", 500, 100, Color.Gold, new Font("Segoe UI", 12.0f));
         bool IsTinting = false;
         float TintVal = 0.0f;
+        Random rnd = new Random();
+        RectangleObject rectangleObject = new RectangleObject(new Rectangle(300, 300, 150, 50), Color.BlueViolet, Color.FromArgb(127, Color.Violet), 2);
 
         public Game(string title = "TestGameApp", int width = 1280, int height = 720) : base(title, width, height)
         {
@@ -35,21 +38,22 @@ namespace testApp
 
         private void Cs_ContentLoading(object sender, ContentLoadingEventArgs e)
         {
-            GameApplication.Log(craftersmine.GameEngine.Utils.LogEntryType.Info, "Loading content from " + e.PackageName + " with name \"" + e.ContentFileName + "\" type of " + e.ContentType.ToString());
+            GameApplication.Log(LogEntryType.Info, "Loading content from " + e.PackageName + " with name \"" + e.ContentFileName + "\" type of " + e.ContentType.ToString());
         }
 
         private void Cs_ContentStorageCreated(object sender, EventArgs e)
         {
-            GameApplication.Log(craftersmine.GameEngine.Utils.LogEntryType.Info, "Creating content storage...");
+            GameApplication.Log(LogEntryType.Info, "Creating content storage...");
         }
 
         Obj1 obj1 = new Obj1();
+        Obj1 obj3 = new Obj1() { X = 400, Y = 500 };
         Obj2 obj2 = new Obj2();
-
-        Label labelDebugKeyboard = new Label();
 
         public override void OnCreated()
         {
+            GameApplication.SetGameFrameTime(16);
+            GameApplication.SetGameTickTime(16);
             GameApplication.SetLogger(new Logger(Path.Combine(GameApplication.AppDataGameRoot, "Logs"), "testGame"));
             GameApplication.Log(LogEntryType.Info, "Initializing Game...");
             //GameClient gameClient = new GameClient(this);
@@ -65,11 +69,11 @@ namespace testApp
             this.AddScene(scene);
             scene.SetBackgroundTexture(cs.LoadTexture("bg", TextureLayout.Stretch));
 
-            scene.Controls.Add(labelDebugKeyboard);
-
             scene.AddGameObject(obj1);
+            scene.AddGameObject(obj3);
             scene.AddGameObject(obj2);
             obj1.ApplyTexture(cs.LoadTexture("obj1", TextureLayout.Tile));
+            obj3.ApplyTexture(cs.LoadTexture("obj1", TextureLayout.Tile));
             obj2.ApplyTexture(cs.LoadTexture("obj2", TextureLayout.Stretch));
 
             obj2.AddAnimation("anim", cs.LoadAnimation("anim"));
@@ -101,6 +105,9 @@ namespace testApp
                 }
                 scene.AddGameObject(bordersGrads[c]);
             }
+            
+            scene.AddRectangle(rectangleObject);
+            scene.AddLabel(label);
             //scene.PlayAudioChannel("aud");
         }
 
@@ -114,8 +121,7 @@ namespace testApp
             Sticks sticks = Gamepad.GetSticks(Player.First);
 
             DPad dPad = Gamepad.GetDPad(Player.First);
-
-            labelDebugKeyboard.Text = Keyboard.GetKeys().ToString();
+            
             Program.labelDebug.Text = $"Current Game Tick: {this.Tick}\r\nLS_X: {sticks.LeftStickAxisX:F2} | LS_Y: {sticks.LeftStickAxisY:F2} | RS_X: {sticks.RightStickAxisX:F2} | RS_Y: {sticks.RightStickAxisY:F2} | LT: {triggers.LT:F2} | RT: {triggers.RT:F2}\r\nButtons: A: {buttons.A} | B: {buttons.B} | X: {buttons.X} | Y: {buttons.Y} | LB: {buttons.LB} | RB: {buttons.RB} | BACK: {buttons.Back} | START: {buttons.Menu} | LS: {buttons.LeftStick} | RS: {buttons.RightStick}\r\nDPad: Up {dPad.Up} | Right: {dPad.Right} | Down: {dPad.Down} | Left: {dPad.Left}\r\nKeyboard: {Keyboard.GetKeys().ToString()}";
 
             obj1.X += (int)(obj1.InitialSpeed * sticks.LeftStickAxisX);
@@ -159,9 +165,6 @@ namespace testApp
                 else obj1.X += obj1.InitialSpeed;
             }
 
-            if (obj1.IsCollided)
-                scene.RemoveGameObject(obj1);
-
             if (GameApplication.GetGameTick() == 100)
             {
                 IsTinting = true;
@@ -184,6 +187,8 @@ namespace testApp
                     scene.TintScene(Color.Black, TintVal);
                 else scene.TintScene(Color.Black, 0.0f);
             }
+            label.LabelColor = Color.FromArgb(rnd.Next(0, 255), rnd.Next(0, 255), rnd.Next(0, 255));
+            rectangleObject.FillColor = Color.FromArgb(rnd.Next(0, 255), rnd.Next(0, 255), rnd.Next(0, 255));
         }
 
         public override void OnExit()
@@ -202,7 +207,7 @@ namespace testApp
             this.Width = 48;
             this.X = 100;
             this.Y = 300;
-            this.IsCollidable = false;
+            this.IsCollidable = true;
             this.SetCollider(4, 4, 24, 24);
             this.IsTransmittingLocation = true;
         }
@@ -213,7 +218,7 @@ namespace testApp
         {
             GameApplication.Log(LogEntryType.Info, this.InternalName + " Collided with " + collidedObject.InternalName, true);
             if (collidedObject.InternalName == "obj2")
-                this.IsCollided = true;
+                Game.scene.RemoveGameObject(this);
         }
 
         public override void OnMouseClick(int xPos, int yPos, MouseButtons mouseButtons)
