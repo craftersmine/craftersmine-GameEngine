81b62ad7f891fc395ccd07e9cd6e52595e7b605e
craftersmine <rodion.morozov.01@gmail.com>
2018-07-17 01:40:42 +0700

    Added static data storage

---
+++ craftersmine.GameEngine.GDK/StaticData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using craftersmine.GDK.Core;
+
+namespace craftersmine.GDK
+{
+    public class StaticData
+    {
+        public static Project LoadedProject { get; set; }
+    }
+}
